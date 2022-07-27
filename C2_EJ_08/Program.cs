0e7ecee0021ad457132095da453433e7dbc18274
Rubirpg <rubi.rpg@gmail.com>
2022-07-27 12:52:53 +0200

    Agregar archivos de proyecto.

---
+++ C2_EJ_08/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2_Ej_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Me dices tu nombre: ");
+            string nombre = Console.ReadLine(); //leemos el nombre y lo guardamos en la variable nombre
+            Console.WriteLine("Me dices tu edad: ");
+            int edad = Convert.ToInt32(Console.ReadLine()); //leemos la ciudad y lo guardamos en la variable edad
+
+            //mostramos por pantalla 
+            Console.WriteLine();
+            Console.WriteLine("Te llamas {0} y tienes {1} años.", nombre, edad);
+        }
+    }
+}
